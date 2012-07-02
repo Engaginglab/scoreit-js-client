8f66d4e30adae9b04b5e43655c78558b04d28f83
Martin Kleinschrodt <martin@maklesoft.com>
2012-07-02 19:52:58 +0200

    list filtering; ApiKey authentication;

--- source/scoreit.js
+++ source/scoreit.js
@@ -1,10 +1,7 @@
 scoreit = {
 	domain: "http://127.0.0.1:8000/",
 	version: "v1",
-	user: {
-		username: "admin",
-		password: "admin"
-	},
+	user: {},
 	//* @protected
 	buildResourceUrl: function(resource, id) {
 		var url = scoreit.domain + "api/" + scoreit.version + "/" + resource + "/";
@@ -23,33 +20,47 @@ scoreit = {
 		Builds an Ajax request. The default implementation uses the enyo framework.
 		Overwrite this method to use a custom implementation.
 	*/
-	request: function(url, method, params, callback) {
-		username = scoreit.user.username;
-		password = scoreit.user.password;
-		var ajax = new enyo.Ajax({url: url, method: method, cacheBust: false, contentType: "application/json", username: username, password: password});
-		ajax.response(callback);
+	request: function(url, method, params, success, failure) {
+		if (scoreit.user.api_key) {
+			params["username"] = scoreit.user.username;
+			params["api_key"] = scoreit.user.api_key;
+		}
+		var ajax = new enyo.Ajax({url: url, method: method, cacheBust: false, contentType: "application/json"});
+		ajax.response(success);
+		ajax.error(failure);
 		ajax.go(params);
 	},
 	requestResource: function(resource, id, method, params, callback) {
 		var url = scoreit.buildResourceUrl(resource, id);
-		console.log(url);
 		scoreit.request(url, method, params, callback);
+	},
+	setUser: function(username, password, callback) {
+		scoreit.request(scoreit.domain + "auth/validate/", "POST", {username: username, password: password}, function(sender, response) {
+			scoreit.user = response;
+			scoreit.user.username = username;
+			callback(true);
+		}, function(sender, response) {
+			document.write(sender.xhr.responseText);
+			callback(false);
+		});
 	}
 };
 
 scoreit.union = {
-	getList: function(filters, callback) {
+	list: function(filters, callback) {
 		var params = {};
-		for (var i = 0; i < filters.length; i++) {
-			var field = filters[i][0];
-			if (filters[i][2]) {
-				field += "__" + filters[i][2];
+		if (filters) {
+			for (var i = 0; i < filters.length; i++) {
+				var field = filters[i][0];
+				if (filters[i][2]) {
+					field += "__" + filters[i][2];
+				}
+				params[field] = filters[i][1];
 			}
-			params[field] = filters[i][1];
 		}
 		scoreit.requestResource("union", null, "GET", params, callback);
 	},
-	getDetail: function(id, callback) {
+	detail: function(id, callback) {
 		scoreit.requestResource("union", id, "GET", null, callback);
 	},
 	create: function(name, callback) {
