a6073428ff76840a351ef8e0fd00f449a6985ee9
Martin Kleinschrodt <martin@maklesoft.com>
2012-07-10 12:09:28 +0200

    Introduce class-based resource interface; implement a few different resources; add a few comments

--- source/scoreit.js
+++ source/scoreit.js
@@ -1,3 +1,6 @@
+/**
+	Javascript client library for the Score.it api
+*/
 scoreit = {
 	domain: "http://127.0.0.1:8000/",
 	version: "v1",
@@ -16,37 +19,65 @@ scoreit = {
 
 		return url;
 	},
-	/*
-		Builds an Ajax request. The default implementation uses the enyo framework.
+	/**
+		Makes an Ajax request. The default implementation uses the enyo framework.
 		Overwrite this method to use a custom implementation.
 	*/
 	request: function(url, method, params, success, failure) {
-		if (scoreit.user.api_key) {
-			params["username"] = scoreit.user.username;
-			params["api_key"] = scoreit.user.api_key;
-		}
+		failure = function(sender, response) {
+			document.write(sender.xhr.responseText);
+		};
+
 		var ajax = new enyo.Ajax({url: url, method: method, cacheBust: false, contentType: "application/json"});
 		ajax.response(success);
 		ajax.error(failure);
 		ajax.go(params);
 	},
-	requestResource: function(resource, id, method, params, callback) {
+	/**
+		Generic function for requestion resources
+	*/
+	requestResource: function(resource, id, method, params, callback, failure) {
 		var url = scoreit.buildResourceUrl(resource, id);
-		scoreit.request(url, method, params, callback);
+
+		params = params || {};
+		if (scoreit.user.api_key) {
+			// params["username"] = scoreit.user.username;
+			// params["api_key"] = scoreit.user.api_key;
+			url += "?username=" + scoreit.user.username + "&api_key=" + scoreit.user.api_key;
+		}
+
+		params = JSON.stringify(params);
+		scoreit.request(url, method, params, callback, failure);
 	},
+	/**
+		Validate user credentials and obtain api key and profile data
+	*/
 	setUser: function(username, password, callback) {
 		scoreit.request(scoreit.domain + "auth/validate/", "POST", {username: username, password: password}, function(sender, response) {
 			scoreit.user = response;
 			scoreit.user.username = username;
 			callback(true);
 		}, function(sender, response) {
-			document.write(sender.xhr.responseText);
 			callback(false);
 		});
 	}
 };
 
-scoreit.union = {
+/*
+	Helper function for prototype inheritance
+*/
+Function.prototype.inheritsFrom = function(parentClass){
+	this.prototype = new parentClass();
+	this.prototype.constructor = this;
+	this.prototype.parent = parentClass.prototype;
+	return this;
+};
+
+/**
+	Base resource class that implements basic REST functionality
+*/
+scoreit.Resource = function() {};
+scoreit.Resource.prototype = {
 	list: function(filters, callback) {
 		var params = {};
 		if (filters) {
@@ -58,18 +89,92 @@ scoreit.union = {
 				params[field] = filters[i][1];
 			}
 		}
-		scoreit.requestResource("union", null, "GET", params, callback);
+		scoreit.requestResource(this.resourceName, null, "GET", params, callback);
 	},
 	detail: function(id, callback) {
-		scoreit.requestResource("union", id, "GET", null, callback);
+		scoreit.requestResource(this.resourceName, id, "GET", null, callback);
 	},
-	create: function(name, callback) {
-		scoreit.requestResource("union", null, "POST", {name: name}, callback);
+	create: function(params, callback) {
+		scoreit.requestResource(this.resourceName, null, "POST", params, callback);
 	},
 	update: function(id, params, callback) {
-		scoreit.requestResource("union", id, "PATCH", params, callback);
+		scoreit.requestResource(this.resourceName, id, "PATCH", params, callback);
 	},
 	remove: function(id, callback) {
-		scoreit.requestResource("union", id, "DELETE", null, callback);
+		scoreit.requestResource(this.resourceName, id, "DELETE", null, callback);
 	}
 };
+
+
+scoreit.Union = function() {
+	this.resourceName = "union";
+};
+
+scoreit.Union.inheritsFrom(scoreit.Resource);
+
+scoreit.Union.prototype.create = function(name, callback) {
+	this.parent.create.call(this, {name: name}, callback);
+};
+
+
+scoreit.League = function() {
+	this.resourceName = "league";
+};
+
+scoreit.League.inheritsFrom(scoreit.Resource);
+
+scoreit.League.prototype.create = function(name, gender, callback) {
+	this.parent.create.call(this, {name: name, gender: gender}, callback);
+};
+
+
+scoreit.Club = function() {
+	this.resourceName = "club";
+};
+
+scoreit.Club.inheritsFrom(scoreit.Resource);
+
+scoreit.Club.prototype.create = function(name, union, callback) {
+	this.parent.create.call(this, {name: name, union: union}, callback);
+};
+
+
+scoreit.Team = function() {
+	this.resourceName = "team";
+};
+
+scoreit.League.inheritsFrom(scoreit.Resource);
+
+scoreit.Team.prototype.create = function(name, club, league, callback) {
+	this.parent.create.call(this, {name: name, club: league}, callback);
+};
+
+
+scoreit.Person = function() {
+	this.resourceName = "person";
+};
+
+scoreit.Person.inheritsFrom(scoreit.Resource());
+
+scoreit.Person.prototype.create = function(firstName, lastName, passNumber, gender, address, city, zipCode, birthday, callback) {
+	this.parent.create.call(this, {first_name: firstName, last_name: lastName,
+		pass_number: passNumber, gender: gender, address: address, city: city, zip_code: zipCode, birthday: birthday}, callback);
+};
+
+
+scoreit.GameType = function() {
+	this.resourceName = "gametype";
+};
+
+scoreit.GameType.inheritsFrom(scoreit.Resource);
+
+scoreit.GameType.prototype.create = function(name, callback) {
+	this.parent.create.call(this, {name: name}, callback);
+};
+
+scoreit.union = new scoreit.Union();
+scoreit.league = new scoreit.League();
+scoreit.club = new scoreit.Club();
+scoreit.team = new scoreit.Team();
+scoreit.person = new scoreit.Person();
+scoreit.gameType = new scoreit.GameType();
