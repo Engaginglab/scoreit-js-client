7e583cc4d0e0d286640e1b1913ec2356e41097c6
Martin Kleinschrodt <martin@maklesoft.com>
2012-07-22 08:59:36 +0200

    Adjust client to new url structure

--- source/scoreit.js
+++ source/scoreit.js
@@ -5,20 +5,6 @@ scoreit = {
 	domain: "http://127.0.0.1:8000/",
 	version: "v1",
 	user: {},
-	//* @protected
-	buildResourceUrl: function(resource, id) {
-		var url = scoreit.domain + "api/" + scoreit.version + "/" + resource + "/";
-
-		if (id !== undefined && id !== null) {
-			if (id.length) {
-				url += "set/" + id.join(";");
-			} else {
-				url += id;
-			}
-		}
-
-		return url;
-	},
 	/**
 		Makes an Ajax request. The default implementation uses the enyo framework.
 		Overwrite this method to use a custom implementation.
@@ -36,8 +22,8 @@ scoreit = {
 	/**
 		Generic function for requestion resources
 	*/
-	requestResource: function(resource, id, method, params, callback, failure) {
-		var url = scoreit.buildResourceUrl(resource, id);
+	requestResource: function(url, method, params, callback, failure) {
+		//var url = scoreit.buildResourceUrl(resource, id);
 
 		params = params || {};
 		if (scoreit.user.api_key) {
@@ -63,14 +49,10 @@ scoreit = {
 			callback(false);
 		});
 	},
-	signUp: function(username, password, email, firstName, lastName, profile, success, fail) {
-		scoreit.request(scoreit.domain + "api/" + scoreit.version + "/auth/signup/", "POST", {
-			username: username, password:password, email: email, first_name: firstName, last_name: lastName, profile: profile
+	signUp: function(username, password, email, firstName, lastName, success, fail) {
+		scoreit.request(scoreit.domain + "auth/api/" + scoreit.version + "/signup/", "POST", {
+			username: username, password:password, email: email, first_name: firstName, last_name: lastName
 		}, success, fail);
-	},
-	isUnique: function(username, email, passNumber, callback) {
-		passNumber = passNumber || 0;
-		scoreit.request(scoreit.domain + "api/" + scoreit.version + "/auth/unique/", "GET", {user_name: username, email: email, pass_number: passNumber}, callback);
 	}
 };
 
@@ -89,6 +71,20 @@ Function.prototype.inheritsFrom = function(parentClass){
 */
 scoreit.Resource = function() {};
 scoreit.Resource.prototype = {
+	//* @protected
+	buildUrl: function(id) {
+		var url = scoreit.domain + this.appPrefix + "api/" + scoreit.version + "/" + this.resourceName + "/";
+
+		if (id !== undefined && id !== null) {
+			if (id.length) {
+				url += "set/" + id.join(";");
+			} else {
+				url += id;
+			}
+		}
+
+		return url;
+	},
 	list: function(filters, callback) {
 		var params = {};
 		if (filters) {
@@ -100,107 +96,150 @@ scoreit.Resource.prototype = {
 				params[field] = filters[i][1];
 			}
 		}
-		scoreit.requestResource(this.resourceName, null, "GET", params, callback);
+		scoreit.requestResource(this.buildUrl(), "GET", params, callback);
 	},
 	detail: function(id, callback) {
-		scoreit.requestResource(this.resourceName, id, "GET", null, callback);
+		scoreit.requestResource(this.buildUrl(id), "GET", null, callback);
 	},
 	create: function(params, callback) {
-		scoreit.requestResource(this.resourceName, null, "POST", params, callback);
+		scoreit.requestResource(this.buildUrl(), "POST", params, callback);
 	},
 	update: function(id, params, callback) {
-		scoreit.requestResource(this.resourceName, id, "PATCH", params, callback);
+		scoreit.requestResource(this.buildUrl(id), "PATCH", params, callback);
 	},
 	put: function(id, params, callback) {
-		scoreit.requestResource(this.resourceName, id, "PUT", params, callback);
+		scoreit.requestResource(this.buildUrl(id), "PUT", params, callback);
 	},
 	remove: function(id, callback) {
-		scoreit.requestResource(this.resourceName, id, "DELETE", null, callback);
+		scoreit.requestResource(this.buildUrl(id), "DELETE", null, callback);
 	}
 };
 
+/*
+	Authentication Resources
+*/
 
-scoreit.Union = function() {
-	this.resourceName = "union";
+scoreit.auth = {};
+
+scoreit.auth.User = function() {
+	this.resourceName = "user";
+	this.appPrefix = "auth/";
 };
 
-scoreit.Union.inheritsFrom(scoreit.Resource);
+scoreit.auth.User.inheritsFrom(scoreit.Resource);
 
-// scoreit.Union.prototype.create = function(name, callback) {
+scoreit.auth.User.prototype.isUnique = function(params, callback) {
+	scoreit.request(scoreit.domain + this.appPrefix + "api/" + scoreit.version + "/unique/", "GET", params, callback);
+};
+
+scoreit.auth.Profile = function() {
+	this.resourceName = "profile";
+	this.appPrefix = "auth/";
+};
+
+scoreit.auth.Profile.inheritsFrom(scoreit.Resource);
+
+scoreit.auth.user = new scoreit.auth.User();
+scoreit.auth.profile = new scoreit.auth.Profile();
+
+/*
+	Handball Resources
+*/
+
+scoreit.handball = {};
+
+scoreit.handball.Union = function() {
+	this.resourceName = "union";
+	this.appPrefix = "handball/";
+};
+
+scoreit.handball.Union.inheritsFrom(scoreit.Resource);
+
+// scoreit.handballUnion.prototype.create = function(name, callback) {
 // 	this.parent.create.call(this, {name: name}, callback);
 // };
 
 
-scoreit.District = function() {
+scoreit.handball.District = function() {
 	this.resourceName = "district";
+	this.appPrefix = "handball/";
 };
 
-scoreit.District.inheritsFrom(scoreit.Resource);
+scoreit.handball.District.inheritsFrom(scoreit.Resource);
 
-// scoreit.League.prototype.create = function(name, gender, callback) {
+// scoreit.handballLeague.prototype.create = function(name, gender, callback) {
 // 	this.parent.create.call(this, {name: name, gender: gender}, callback);
 // };
 
 
-scoreit.League = function() {
+scoreit.handball.League = function() {
 	this.resourceName = "league";
+	this.appPrefix = "handball/";
 };
 
-scoreit.League.inheritsFrom(scoreit.Resource);
+scoreit.handball.League.inheritsFrom(scoreit.Resource);
 
-// scoreit.League.prototype.create = function(name, gender, callback) {
+// scoreit.handball.League.prototype.create = function(name, gender, callback) {
 // 	this.parent.create.call(this, {name: name, gender: gender}, callback);
 // };
 
 
-scoreit.Club = function() {
+scoreit.handball.Club = function() {
 	this.resourceName = "club";
+	this.appPrefix = "handball/";
 };
 
-scoreit.Club.inheritsFrom(scoreit.Resource);
+scoreit.handball.Club.inheritsFrom(scoreit.Resource);
 
-// scoreit.Club.prototype.create = function(name, union, callback) {
+// scoreit.handball.Club.prototype.create = function(name, union, callback) {
 // 	this.parent.create.call(this, {name: name, union: union}, callback);
 // };
 
 
-scoreit.Team = function() {
+scoreit.handball.Team = function() {
 	this.resourceName = "team";
+	this.appPrefix = "handball/";
 };
 
-scoreit.Team.inheritsFrom(scoreit.Resource);
+scoreit.handball.Team.inheritsFrom(scoreit.Resource);
 
-// scoreit.Team.prototype.create = function(name, club, league, callback) {
+// scoreit.handball.Team.prototype.create = function(name, club, league, callback) {
 // 	this.parent.create.call(this, {name: name, club: league}, callback);
 // };
 
 
-scoreit.Person = function() {
+scoreit.handball.Person = function() {
 	this.resourceName = "person";
+	this.appPrefix = "handball/";
 };
 
-scoreit.Person.inheritsFrom(scoreit.Resource);
+scoreit.handball.Person.inheritsFrom(scoreit.Resource);
 
-// scoreit.Person.prototype.create = function(firstName, lastName, passNumber, gender, address, city, zipCode, birthday, callback) {
+scoreit.handball.Person.prototype.isUnique =  function(params, callback) {
+	scoreit.request(scoreit.domain + this.appPrefix + "api/" + scoreit.version + "/unique/", "GET", params, callback);
+};
+
+// scoreit.handball.Person.prototype.create = function(firstName, lastName, passNumber, gender, address, city, zipCode, birthday, callback) {
 // 	this.parent.create.call(this, {first_name: firstName, last_name: lastName,
 // 		pass_number: passNumber, gender: gender, address: address, city: city, zip_code: zipCode, birthday: birthday}, callback);
 // };
 
 
-scoreit.GameType = function() {
+scoreit.handball.GameType = function() {
 	this.resourceName = "gametype";
+	this.appPrefix = "handball/";
 };
 
-scoreit.GameType.inheritsFrom(scoreit.Resource);
+scoreit.handball.GameType.inheritsFrom(scoreit.Resource);
 
-// scoreit.GameType.prototype.create = function(name, callback) {
+// scoreit.handball.GameType.prototype.create = function(name, callback) {
 // 	this.parent.create.call(this, {name: name}, callback);
 // };
 
-scoreit.union = new scoreit.Union();
-scoreit.district = new scoreit.District();
-scoreit.league = new scoreit.League();
-scoreit.club = new scoreit.Club();
-scoreit.team = new scoreit.Team();
-scoreit.person = new scoreit.Person();
-scoreit.gameType = new scoreit.GameType();
+scoreit.handball.union = new scoreit.handball.Union();
+scoreit.handball.district = new scoreit.handball.District();
+scoreit.handball.league = new scoreit.handball.League();
+scoreit.handball.club = new scoreit.handball.Club();
+scoreit.handball.team = new scoreit.handball.Team();
+scoreit.handball.person = new scoreit.handball.Person();
+scoreit.handball.gameType = new scoreit.handball.GameType();
